b974abb8b64333312323769889341382a564f439
SafarRadim <safar.radim@gmail.com>
2019-06-13 14:30:13 +0200

    reworked sqlalchemy to flask-sqlalchemy

--- datastore/db_source_old.py
+++ datastore/db_source_old.py
@@ -1,150 +1,150 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy as sql
+import sqlalchemy as sqla
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
 
 import datastore.db_packages as pk
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///datastore/dbfile.db'
-db = sql(app)
+metadata = sqla.MetaData()
+Base = declarative_base()
 
 
-class Ecosystem(db.Model):
+class Ecosystem(Base):
     __tablename__ = 'ecosystem'
+    id = sqla.Column(sqla.Integer, primary_key=True)
+    name = sqla.Column(sqla.String)
+    packages = relationship('Packages', backref='eco')
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    packages = sql.relationship('Packages', backref='eco')
 
-
-class Packages(db.Model):
-
+class Packages(Base):
     __tablename__ = 'packages'
-    id = db.Column(db.Integer, primary_key=True)
-    eco_id = db.Column(db.Integer, db.ForeignKey('ecosystem.id'))
-    name = db.Column(db.String)
-    description = db.Column(db.String)
-    repo = db.Column(db.String)
-    versions = sql.relationship('Versions', backref='pack')
+    id = sqla.Column(sqla.Integer, primary_key=True)
+    eco_id = sqla.Column(sqla.Integer, sqla.ForeignKey('ecosystem.id'))
+    name = sqla.Column(sqla.String)
+    description = sqla.Column(sqla.String)
+    repo = sqla.Column(sqla.String)
+    versions = relationship('Versions', backref='pack')
 
 
-class Versions(db.Model):
+class Versions(Base):
     __tablename__ = 'versions'
-
-    id = db.Column(db.Integer, primary_key=True)
-    version = db.Column(db.String)
-    package = db.Column(db.String, db.ForeignKey('packages.name'))
+    id = sqla.Column(sqla.Integer, primary_key=True)
+    version = sqla.Column(sqla.String)
+    package = sqla.Column(sqla.String, sqla.ForeignKey('packages.name'))
 
 
 class SqlalchemyDatabase(object):
     def __init__(self, backend):
-        db.Model.metadata.create_all()
-        self.session = db.create_scoped_session()
+        engine = sqla.create_engine(backend, echo=False)
+        Base.metadata.create_all(engine)
+        self.Session = sessionmaker(bind=engine)
+        session = self.Session()
 
     def store(self, *args):
-        session = self.session
+        session = self.Session()
         package_in_db = None
         for package in args:
+
             try:
                 package_to_dict = package.to_dict()
                 if 'eco' in package_to_dict:
                     exists = self.restore_from_table(package_to_dict['name'], 'packages')
                     if exists is None:
-                        pass
-
+                        eco = session.query(Ecosystem).filter_by(name=package_to_dict['eco']).first()
+                        package_to_dict['eco'] = eco
+                        package_in_db = Packages(**package_to_dict)
+                        print(package_to_dict)
                 elif 'pack' in package_to_dict:
                     exists = self.restore_from_table(package_to_dict['version'], 'versions')
                     if exists is None:
-                        pass
-
+                        pack = session.query(Packages).filter_by(name=package_to_dict['pack']).first()
+                        package_to_dict['pack'] = pack
+                        package_in_db = Versions(**package_to_dict)
                 elif 'name' in package_to_dict:
                     exists = self.restore_from_table(package_to_dict['name'], 'ecosystem')
                     if exists is None:
-                        pass
-
+                        package_in_db = Ecosystem(**package_to_dict)
+                else:
+                    print('Bad Arguments')
+                if package_in_db is not None:
+                    session.add(package_in_db)
             except Exception as e:
                 print(e)
         session.commit()
 
     def restore_all(self, table):
-        session = self.session
+        session = self.Session()
         item = []
         try:
             if table.title() == 'Ecosystem':
-                items = Ecosystem.query.all()
+                items = session.query(Ecosystem).all()
                 for i in items:
                     item.append(pk.Package(i.name))
-
             elif table.title() == 'Packages':
-                items = Packages.query.all()
+                items = session.query(Packages).all()
                 for i in items:
                     item.append(pk.Description(i.name, i.description, i.repo, i.eco))
-
             elif table.title() == 'Versions':
-                items = Versions.quary.all()
+                items = session.query(Versions).all()
                 for i in items:
                     item.append(pk.Version(i.version, i.package))
-
             return item
-
         except Exception as e:
             print(e)
 
     def restore_from_table(self, name, table):
-        session = self.session
+        session = self.Session()
         try:
             if table.title() == 'Ecosystem':
-                eco = Ecosystem.query.filter_by(name=name).first()
+                eco = session.query(Ecosystem).filter_by(name=name).first()
                 if eco is None:
                     return None
                 package = pk.Package(eco.name)
-
             elif table.title() == 'Packages':
-                pack = Packages.query.filter_by(name=name).first()
+                pack = session.query(Packages).filter_by(name=name).first()
                 if pack is None:
                     return None
-
+                package = pk.Description(pack.name, pack.description, pack.repo, pack.eco)
             elif table.title() == 'Versions':
-                ver = Versions.query.filter_by(version=name).first()
+                ver = session.query(Versions).filter_by(version=name).first()
                 if ver is None:
                     return None
                 package = pk.Version(ver.version, ver.package)
-
             else:
                 print('Badly defined table to add to')
-
             return package
-
         except Exception as e:
             print(e)
 
     def restore_from_master(self, name, master_table):
-        session = self.session
+        session = self.Session()
         packages = []
 
         try:
             if master_table.title() == 'Ecosystem':
-                package = Ecosystem.query.filter_by(name=name).first()
+                package = session.query(Ecosystem).filter_by(name=name).first()
                 if package is None:
                     return None
                 id = package.id
-                packs_from_db = Packages.query.filter_by(eco_id=id).all()
+                packs_from_db = session.query(Packages).filter_by(eco_id=id).all()
                 for pack in packs_from_db:
                     package = pk.Description(pack.name, pack.description, pack.repo, pack.eco)
                     packages.append(package)
-            elif master_table.title() == 'Package':
-                package = Packages.query.filter_by(name=name).first()
+            elif master_table.title() == 'Packages':
+                package = session.query(Packages).filter_by(name=name).first()
                 if package is None:
                     return None
                 name = package.name
-                vers = Versions.query.filter_by(package=name).all()
+                vers = session.query(Versions).filter_by(package=name).all()
                 for ver in vers:
                     version = pk.Version(ver.version, ver.package)
                     packages.append(version)
-
             else:
                 print('Bad master table')
-
             return packages
-
         except Exception as e:
             print(e)
+
+
+def CreateDatebase():
+    database = SqlalchemyDatabase('sqlite:///dbfile.db')
+    return database
